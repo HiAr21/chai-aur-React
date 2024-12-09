e25fa5c8b59322fa1be7ff382557b780fd8a31e3
Himanshu Arya <himanshuarya0000012@gmail.com>
2024-12-09 21:25:05 +0530

    react flow and structure | convention (cap. initial)

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Dinner from "./DinnerMessage"
+
+function App() {
+  
+  return (
+    <>
+      <h1>Chai aur react with vite.</h1> 
+      <Dinner/>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/DinnerMessage.jsx
@@ -0,0 +1,7 @@
+export default function Dinner(){
+    return(
+        <>
+            <h1>dinner with Reactjs</h1>
+        </>
+    )
+}
